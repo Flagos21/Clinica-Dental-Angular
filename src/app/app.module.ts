762eb1f295c714cbc30eddee032131d8813321c1
Fabian <flagosp21@gmail.com>
2023-10-25 11:58:33 -0300

    Login1Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { AngularFireModule} from '@angular/fire/compat'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { LoginComponent } from './Components/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PagenotfoundComponent } from './Components/pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({ 
+      apiKey: "AIzaSyBuIaTekNyJbpBDINZHEtY1WLGqvX18pss",
+      authDomain: "clinicadental-808bd.firebaseapp.com",
+      projectId: "clinicadental-808bd",
+      storageBucket: "clinicadental-808bd.appspot.com",
+      messagingSenderId: "981730374950",
+      appId: "1:981730374950:web:2f7d3b952c4e00169a3de7",
+      measurementId: "G-9895JQMZMJ"
+    }),
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
