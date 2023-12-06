022d2eb9a0fb3b103f8b298b64a81ea709f95991
PedroCD520 <133519915+PedroCD520@users.noreply.github.com>
2023-12-05 22:42:39 -0300

    hora usuario

--- src/app/Components/intro/intro.component.ts
+++ src/app/Components/intro/intro.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-intro',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./intro.component.css']
 })
 export class IntroComponent {
+  mostrarSoloHoras(event: any) {
+    const horaInput = event.target.value; // Obtener el valor del input
+    const [horas, minutos] = horaInput.split(':'); // Separar las horas y los minutos
 
+    // Modificar el valor del input para mostrar solo las horas (formato HH:00)
+    event.target.value = `${horas}:00`;
+  }
 }
